ce906cd6cd4eb0d063c31058e69b18f8ddff4484
emiesuhaemi21 <emiesuhaemi21@gmail.com>
2026-05-23 10:12:10 +0700

    deploy build out

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/PT.-PJE",
+  assetPrefix: "/PT.-PJE/",
+};
+
+module.exports = nextConfig;
